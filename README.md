a5f55f17c60e3ea9dc24568658bf812607d0e2c6
Dipta Das <dipta670@gmail.com>
2019-11-11 00:00:19 -0600

    Added ribbon demo

--- README.md
+++ README.md
@@ -25,3 +25,21 @@ $ openssl rsa -in server.key -out server.key // remove passphrase
 
 $ openssl x509 -req -days 365 -in server.csr -signkey server.key -out server.crt // generate self signed certificate
 ```
+
+## Client Side LB: Ribbon
+
+### Spring boot demo server and client (with Ribbon) 
+
+```
+$ cd ribbon-demo
+$ mvn clean install
+
+// run server
+$ cd demo-server
+$ java -jar -Dserver.port=8070 target/demo-server-0.0.1-SNAPSHOT.jar
+$ java -jar -Dserver.port=8090 target/demo-server-0.0.1-SNAPSHOT.jar
+
+// run client
+$ cd ../demo-client
+$ java -jar target/demo-client-0.0.1-SNAPSHOT.jar
+```
---
+++ ribbon-demo/demo-client/src/main/java/edu/baylor/ecs/client/DemoClientApplication.java
@@ -0,0 +1,35 @@
+package edu.baylor.ecs.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@SpringBootApplication
+@RibbonClient(name = "demo-server", configuration = DemoServerConfiguration.class)
+public class DemoClientApplication {
+    @Bean
+    @LoadBalanced
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @RequestMapping()
+    public String sendRequest() {
+        return this.restTemplate.getForObject("http://demo-server", String.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoClientApplication.class, args);
+    }
+
+}
